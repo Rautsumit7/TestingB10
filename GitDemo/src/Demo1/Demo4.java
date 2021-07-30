4b6ec89dfc4afaeb4cdc099771dc4a7cc223bcec
Ashish3915 <Ashish3915@Ashish>
2021-07-30 16:11:23 +0530

    Second class Demo4 By Sumit

---
+++ GitDemo/src/Demo1/Demo4.java
@@ -0,0 +1,14 @@
+package Demo1;
+
+public class Demo4 {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello sumit");
+		
+		
+	}
+		public void Test()
+		{
+			System.out.println("Test");
+		}
+}
